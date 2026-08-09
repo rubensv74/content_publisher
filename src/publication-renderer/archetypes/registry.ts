c5f43a8ce8bb6eb5e46ea8e1a40303c9eb7a7f39
Rubén <seijoruben@gmail.com>
2026-08-09 16:42:56 +0200

    feat: register Split Screenshot archetype

--- src/publication-renderer/archetypes/registry.ts
+++ src/publication-renderer/archetypes/registry.ts
@@ -2,12 +2,14 @@ import { boldStatementDefinition } from "./bold-statement/definition";
 import { buildNoteDefinition } from "./build-note/definition";
 import { heroScreenshotDefinition } from "./hero-screenshot/definition";
 import { processStepsDefinition } from "./process-steps/definition";
+import { splitScreenshotDefinition } from "./split-screenshot/definition";
 import { stepByStepDefinition } from "./step-by-step/definition";
 
 export const publicationArchetypes = [
   buildNoteDefinition,
   boldStatementDefinition,
   heroScreenshotDefinition,
+  splitScreenshotDefinition,
   processStepsDefinition,
   stepByStepDefinition,
 ] as const;
